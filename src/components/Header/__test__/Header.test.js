b5ecc033668acd674287ddd759f805e2254ac039
Aleksander Chojnowski <achojnowski00@gmail.com>
2023-03-16 14:30:56 +0100

    Assertions

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -7,30 +7,30 @@ test("should render same text passed into title prop", () => {
   expect(headerElement).toBeInTheDocument();
 });
 
-test("should render by Role", () => {
-  render(<Header title="My header" />);
-  const headerElement = screen.getByRole("heading", { name: "My header" });
-  expect(headerElement).toBeInTheDocument();
-});
+// test("should render by Role", () => {
+//   render(<Header title="My header" />);
+//   const headerElement = screen.getByRole("heading", { name: "My header" });
+//   expect(headerElement).toBeInTheDocument();
+// });
 
-test("should render elements by title", () => {
-  render(<Header title="My header" />);
-  const headerElement = screen.getAllByTitle("Header");
-  headerElement.forEach((element) => {
-    expect(element).toBeInTheDocument();
-  });
-});
+// test("should render elements by title", () => {
+//   render(<Header title="My header" />);
+//   const headerElement = screen.getAllByTitle("Header");
+//   headerElement.forEach((element) => {
+//     expect(element).toBeInTheDocument();
+//   });
+// });
 
-// FIND BY
-test("should render same text passed into title prop with findByText", async () => {
-  render(<Header title="Testing header" />);
-  const headerElement = await screen.findByText(/Testing header/i);
-  expect(headerElement).toBeInTheDocument();
-});
+// // FIND BY
+// test("should render same text passed into title prop with findByText", async () => {
+//   render(<Header title="Testing header" />);
+//   const headerElement = await screen.findByText(/Testing header/i);
+//   expect(headerElement).toBeInTheDocument();
+// });
 
-// QUERY BY
-test("should render same text passed into title prop with queryByText", () => {
-  render(<Header title="Testing header" />);
-  const headerElement = screen.queryByText(/dogs/i);
-  expect(headerElement).not.toBeInTheDocument();
-});
+// // QUERY BY
+// test("should render same text passed into title prop with queryByText", () => {
+//   render(<Header title="Testing header" />);
+//   const headerElement = screen.queryByText(/dogs/i);
+//   expect(headerElement).not.toBeInTheDocument();
+// });
---
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import TodoFooter from "../TodoFooter";
+import { BrowserRouter } from "react-router-dom";
+
+const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
+  return (
+    <BrowserRouter>
+      <TodoFooter numberOfIncompleteTasks={numberOfIncompleteTasks} />
+    </BrowserRouter>
+  );
+};
+
+test("should render number of incomplete tasks", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+  const paragraphElement = screen.getByText(/5 tasks left/i);
+  expect(paragraphElement).toBeInTheDocument();
+});
+
+test("should render 'task' when numberOfIncompleteTasks is 1", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toBeInTheDocument();
+});
