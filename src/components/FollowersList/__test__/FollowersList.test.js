62b1cc54383af76476600de619ec1ebc735d36d6
Aleksander Chojnowski <achojnowski00@gmail.com>
2023-03-17 12:45:35 +0100

    Async elements, integration with API

---
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import FollowersList from "../FollowersList";
+
+const MockFollowersList = () => {
+  return (
+    <BrowserRouter>
+      <FollowersList />
+    </BrowserRouter>
+  );
+};
+
+describe("Followerslist tests", () => {
+  test("should render first follower on list", async () => {
+    render(<MockFollowersList />);
+    const followersElements = await screen.findByTestId("follower-item-0");
+    expect(followersElements).toBeInTheDocument();
+  });
+
+  test("should render 5 followers in list", async () => {
+    render(<MockFollowersList />);
+    const followersElements = await screen.findAllByTestId(/follower-item/i);
+    expect(followersElements.length).toBe(5);
+  });
+});
