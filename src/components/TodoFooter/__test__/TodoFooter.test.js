adf987ce5982a054854a1274f6f40017af8b24c7
Aleksander Chojnowski <achojnowski00@gmail.com>
2023-03-17 11:19:25 +0100

    describe block

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -10,14 +10,16 @@ const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
   );
 };
 
-test("should render number of incomplete tasks", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={5} />);
-  const paragraphElement = screen.getByText(/5 tasks left/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
+describe("TodoFooter describe block", () => {
+  test("should render number of incomplete tasks", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
 
-test("should render 'task' when numberOfIncompleteTasks is 1", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeInTheDocument();
+  test("should render 'task' when numberOfIncompleteTasks is 1", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
 });
